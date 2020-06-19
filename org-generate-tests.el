dc851b4d69b2d5ce262bbba74ea011967f8dcbb7
conao3 <conao3@gmail.com>
2020-06-20 05:50:28 +0900

    add test

--- org-generate-tests.el
+++ org-generate-tests.el
@@ -28,6 +28,92 @@
 (require 'cort)
 (require 'org-generate)
 
+(cort-deftest org-generate/simple
+  (cort-generate :equal
+    '(((+ 2 3) 5))))
+
+(cort-deftest org-generate/onefile
+  (cort-generate-with-hook :equal
+    (lambda ()
+      (setq org-generate/onefile/dir
+            (expand-file-name
+             (format "org-generate-%04d" (random (round 1e4)))
+             temporary-file-directory))
+      (mkdir org-generate/onefile/dir))
+    (lambda ()
+      (ignore-errors
+        (delete-directory org-generate/onefile/dir 'force)))
+    '(((let ((org-generate--file-buffer
+              (get-buffer-create "*org-generate*")))
+         (with-current-buffer org-generate--file-buffer
+           (erase-buffer)
+           (insert "\
+* hugo
+** page
+#+begin_src markdown
+  ---
+  title: \"xxx\"
+  ---
+
+  ### 1. First
+  xxxx
+#+end_src
+")
+           (buffer-string)))
+       "\
+* hugo
+** page
+#+begin_src markdown
+  ---
+  title: \"xxx\"
+  ---
+
+  ### 1. First
+  xxxx
+#+end_src
+")
+
+      ((let ((org-generate-root org-generate/onefile/dir)
+             (org-generate--file-buffer
+              (get-buffer-create "*org-generate*")))
+         (with-current-buffer org-generate--file-buffer
+           (erase-buffer)
+           (insert "\
+* hugo
+** page
+*** page
+#+begin_src markdown
+  ---
+  title: \"xxx\"
+  date: xx/xx/xx
+  draft: true
+  ---
+
+  ### 1. First
+  xxxx
+
+  ### 2. Second
+  yyyy
+#+end_src
+")
+           (org-generate "hugo/page")
+           (with-temp-buffer
+             (insert-file-contents
+              (expand-file-name "page" org-generate/onefile/dir))
+             (buffer-string))))
+       "\
+---
+title: \"xxx\"
+date: xx/xx/xx
+draft: true
+---
+
+### 1. First
+xxxx
+
+### 2. Second
+yyyy
+"))))
 
 ;; (provide 'org-generate-tests)
 
