31bf91aaf5b67c93afe17a46c9121862b564047f
conao3 <conao3@gmail.com>
2020-07-07 09:15:16 +0900

    tagged v1.0.1

--- org-generate.el
+++ org-generate.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020  Naoya Yamashita
 
 ;; Author: Naoya Yamashita <conao3@gmail.com>
-;; Version: 0.0.1
+;; Version: 1.0.1
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1") (org "9.3") (mustache "0.23"))
 ;; URL: https://github.com/conao3/org-generate.el
